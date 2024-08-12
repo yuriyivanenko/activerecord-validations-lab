646bc67339be78c612e4ad4f99ddc7e9861476e1
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-08-12 12:53:01 -0400

    Pass all tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  validate :non_clickbait
+
+  private
+
+  def non_clickbait
+    clickbait_patterns = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d+/i,
+      /Guess/i
+    ]
+
+    unless clickbait_patterns.any? { |pattern| pattern.match?(title) }
+      errors.add(:title, "must be clickbait-y")
+    end
+  end
 end
